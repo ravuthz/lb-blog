cffa8e93de8cc364ef555b32df01c6a0d41deb94
Yo Vannaravuth <ravuthz@gmail.com>
2017-09-08 06:01:58 +0000

    Connect mongodb to mlab

--- server/boot/migrate-tables.js
+++ server/boot/migrate-tables.js
@@ -2,32 +2,32 @@ module.exports = function(app) {
   
   console.log("Migrate DB");
   
-  app.dataSources.mysqlDs.automigrate('User', function(err) {
+  app.dataSources.mongoDs.automigrate('User', function(err) {
     if (err) throw err;
     console.log("User was migrated");
   });
   
-  app.dataSources.mysqlDs.automigrate('Role', function(err) {
+  app.dataSources.mongoDs.automigrate('Role', function(err) {
     if (err) throw err;
     console.log("Role was migrated");
   });
   
-  app.dataSources.mysqlDs.automigrate('Post', function(err) {
+  app.dataSources.mongoDs.automigrate('Post', function(err) {
     if (err) throw err;
     console.log("Post was migrated");
   });
   
-  app.dataSources.mysqlDs.automigrate('ACL', function(err) {
+  app.dataSources.mongoDs.automigrate('ACL', function(err) {
     if (err) throw err;
     console.log("ACL was migrated");
   });
   
-  app.dataSources.mysqlDs.automigrate('AccessToken', function(err) {
+  app.dataSources.mongoDs.automigrate('AccessToken', function(err) {
     if (err) throw err;
     console.log("AccessToken was migrated");
   });
   
-  app.dataSources.mysqlDs.automigrate('RoleMapping', function(err) {
+  app.dataSources.mongoDs.automigrate('RoleMapping', function(err) {
     if (err) throw err;
     console.log("RoleMapping was migrated");
   });

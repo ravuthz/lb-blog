c77b99b0d8344513f90888b5368709da5f9998b3
Yo Vannaravuth <ravuthz@gmail.com>
2017-09-08 06:32:45 +0000

    Push to github

--- server/boot/migrate-tables.js
+++ server/boot/migrate-tables.js
@@ -2,32 +2,34 @@ module.exports = function(app) {
   
   console.log("Migrate DB");
   
-  app.dataSources.mysqlDs.automigrate('User', function(err) {
+  var datasource = app.dataSources.mysqlDs;
+  
+  datasource.automigrate('User', function(err) {
     if (err) throw err;
     console.log("User was migrated");
   });
   
-  app.dataSources.mysqlDs.automigrate('Role', function(err) {
+  datasource.automigrate('Role', function(err) {
     if (err) throw err;
     console.log("Role was migrated");
   });
   
-  app.dataSources.mysqlDs.automigrate('Post', function(err) {
+  datasource.automigrate('Post', function(err) {
     if (err) throw err;
     console.log("Post was migrated");
   });
   
-  app.dataSources.mysqlDs.automigrate('ACL', function(err) {
+  datasource.automigrate('ACL', function(err) {
     if (err) throw err;
     console.log("ACL was migrated");
   });
   
-  app.dataSources.mysqlDs.automigrate('AccessToken', function(err) {
+  datasource.automigrate('AccessToken', function(err) {
     if (err) throw err;
     console.log("AccessToken was migrated");
   });
   
-  app.dataSources.mysqlDs.automigrate('RoleMapping', function(err) {
+  datasource.automigrate('RoleMapping', function(err) {
     if (err) throw err;
     console.log("RoleMapping was migrated");
   });

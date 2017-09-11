3c6185b55e25610833be9d95150f0c8a476bb5e5
Ravuthz <ravuthz@gmail.com>
2017-09-11 10:22:31 +0700

    Format 4 Space

--- server/boot/migrate-tables.js
+++ server/boot/migrate-tables.js
@@ -1,37 +1,20 @@
 module.exports = function(app) {
-  
-  console.log("Migrate DB");
-  
-  var datasource = app.dataSources.mongoDs;
-  
-  datasource.automigrate('User', function(err) {
-    if (err) throw err;
-    console.log("User was migrated");
-  });
-  
-  datasource.automigrate('Role', function(err) {
-    if (err) throw err;
-    console.log("Role was migrated");
-  });
-  
-  datasource.automigrate('Post', function(err) {
-    if (err) throw err;
-    console.log("Post was migrated");
-  });
-  
-  datasource.automigrate('ACL', function(err) {
-    if (err) throw err;
-    console.log("ACL was migrated");
-  });
-  
-  datasource.automigrate('AccessToken', function(err) {
-    if (err) throw err;
-    console.log("AccessToken was migrated");
-  });
-  
-  datasource.automigrate('RoleMapping', function(err) {
-    if (err) throw err;
-    console.log("RoleMapping was migrated");
-  });
-  
+
+    console.log("Migrate DB");
+
+    var allModels = ['User', 'Role', 'Post', 'ACL', 'AccessToken', 'RoleMapping'];
+
+    var datasource = app.dataSources.mongoDs;
+
+    datasource.isActual(allModels, function(error, actual) {
+        if (!actual) {
+            datasource.autoupdate(allModels, function(err) {
+                if (err) {
+                    console.log(err);
+                    throw err;
+                }
+            });
+        }
+    });
+
 };

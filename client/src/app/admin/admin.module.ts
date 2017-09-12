7c414d3e4942e447b020559312c192d916d88f9f
Ravuthz <ravuthz@gmail.com>
2017-09-13 01:05:32 +0700

    Add posts list with pagination basic

--- client/src/app/admin/admin.module.ts
+++ client/src/app/admin/admin.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { PaginationModule, PaginationComponent } from 'ngx-bootstrap/pagination';
 
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
@@ -7,7 +12,12 @@ import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
     imports: [
-        CommonModule
+        HttpModule,
+        FormsModule,
+        RouterModule,
+        CommonModule,
+        HttpClientModule,
+        PaginationModule.forRoot()
     ],
     declarations: [
         HomeComponent,
--- client/src/app/admin/posts/posts.component.ts
+++ client/src/app/admin/posts/posts.component.ts
@@ -1,15 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-posts',
-  templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.css']
+    selector: 'app-posts',
+    templateUrl: './posts.component.html',
+    styleUrls: ['./posts.component.css']
 })
 export class PostsComponent implements OnInit {
 
-  constructor() { }
+    private link: string = "http://all-node-ravuthz.c9users.io:8080/api/posts";
+    private posts;
 
-  ngOnInit() {
-  }
+    public totalItems: number = 100;
+    public currentPage: number = 1;
+    public itemsPerPage: number = 10;
+    public smallNumPages: number = 0;
+    public boundaryLinks: boolean = true;
+    public directionLinks: boolean = true;
+
+    public currentSize: number = 10;
+    public sizeList: Array<any> = [10, 15, 20, 25, 50, 100];
+
+    constructor(private http: HttpClient) { }
+
+    ngOnInit() {
+        this.loadPosts(this.currentPage, this.itemsPerPage);
+    }
+
+    public loadPosts(pageNum, perPage) {
+        var gotoPage = (perPage * (pageNum - 1));
+        var filter = "?filter[limit]=" + perPage;
+        filter += "&filter[skip]=" + gotoPage;
+        filter += "&filter[order]=id,asc";
+        this.http.get(this.link + filter).subscribe(data => {
+            this.posts = data;
+        });
+        this.http.get(this.link + "/count").subscribe(data => {
+            this.totalItems = data['count'];
+        });
+    }
+
+    public pageChanged(event: any): void {
+        console.log('Page changed to: ' + event.page);
+        console.log('Number items per page: ' + event.itemsPerPage);
+
+        this.currentPage = event.page;
+        this.itemsPerPage = event.itemsPerPage;
+
+        this.loadPosts(this.currentPage, this.itemsPerPage);
+    }
+
+    public sizeChanged(event: any): void {
+        console.log('Event : ' + event);
+        console.log('Size changed to: ' + this.currentSize);
+
+        if (this.currentPage > this.totalItems / this.itemsPerPage) {
+            this.currentPage = (this.totalItems / this.itemsPerPage);
+        }
+
+        this.loadPosts(this.currentPage, this.itemsPerPage);
+    }
 
 }
